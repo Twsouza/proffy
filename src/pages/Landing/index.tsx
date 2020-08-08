3b4329acd0620439fbba2831f9c00a8e27510feb
Taynan Souza <taynan.tws@gmail.com>
2020-08-08 19:58:16 -0300

    Adding Routes to the App
    Routes added: Index, TeacherList and TeacherForm

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
 import studyIcon from '../../assets/images/icons/study.svg';
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
+
 import './styles.css'
 
 function Landing() {
@@ -18,14 +21,14 @@ function Landing() {
         <img src={landingImg} alt="Plataforma de estudos" className="hero-image"/>
 
         <div className="buttons-container">
-          <a href="#test1" className="study">
+          <Link to="/study" className="study">
             <img src={studyIcon} alt="Estudar"/>
             Estudar
-          </a>
-          <a href="#test" className="give-classes">
+          </Link>
+          <Link to="/give-classes" className="give-classes">
             <img src={giveClassesIcon} alt="Dar aulas"/>
             Dar aulas
-          </a>
+          </Link>
         </div>
 
         <span className="total-connections">
